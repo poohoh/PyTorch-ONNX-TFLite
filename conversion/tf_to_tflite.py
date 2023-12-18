635cbbb29d455df66d9c0226ccce7d532ada94a8
poohoh <kjh980328@gmail.com>
2023-12-18 18:01:02 +0900

    set fp16

--- conversion/tf_to_tflite.py
+++ conversion/tf_to_tflite.py
@@ -2,14 +2,20 @@ import os
 
 import tensorflow as tf
 
-saved_model_dir = '../../model/230914_vd/TF'
-tflite_save_path =  '../../model/230914_vd/TFLite'
-tflite_model_path = os.path.join(tflite_save_path, 'vd.tflite')
+saved_model_dir = '../../model/230918_lpr/TF'
+tflite_save_path =  '../../model/230918_lpr/TFLite_FP16'
+tflite_model_path = os.path.join(tflite_save_path, 'lpr_fp16.tflite')
 
 os.makedirs(tflite_save_path)
 
-# Convert the model
+# set converter
 converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
+
+# set fp16
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.target_spec.supported_types = [tf.float16]
+
+# convert model
 tflite_model = converter.convert()
 
 # Save the model
